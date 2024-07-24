6b28148a754bc346cea957d1872f9a5f3229f829
Alexandr Fonov <FonovAD@Gmail.com>
2024-07-24 10:21:22 +0300

    test pkg/db/db_test.go|errors.go
    db_test.go: HashTest - add errors check
    errors.go: ErrKeyTooLong

--- pkg/db/db_test.go
+++ pkg/db/db_test.go
@@ -9,46 +9,53 @@ import (
 
 func HashTest(t *testing.T) {
 	type TestCase struct {
-		ID     int
-		Name   string
-		Key    string
-		dbSize int
+		ID       int
+		Name     string
+		Key      string
+		dbSize   int
+		Expected error
 	}
 	tcs := []TestCase{
 		TestCase{
-			ID:     1,
-			Name:   "basic case, db_size = 100",
-			Key:    "FirstTest",
-			dbSize: 100,
+			ID:       1,
+			Name:     "basic case, db_size = 100",
+			Key:      "FirstTest",
+			dbSize:   100,
+			Expected: nil,
 		},
 		TestCase{
-			ID:     2,
-			Name:   "basic case, db_size = 1000",
-			Key:    "SecondTest",
-			dbSize: 1000,
+			ID:       2,
+			Name:     "basic case, db_size = 1000",
+			Key:      "SecondTest",
+			dbSize:   1000,
+			Expected: nil,
 		},
 		TestCase{
-			ID:     3,
-			Name:   "Empty key",
-			Key:    "",
-			dbSize: 100,
+			ID:       3,
+			Name:     "Empty key",
+			Key:      "",
+			dbSize:   100,
+			Expected: db.ErrInvalidKey,
 		},
 		TestCase{
-			ID:     4,
-			Name:   "Too long key",
-			Key:    "qwertyuiop[]asdfghjkl;'zxcvbnm,./qwertyuiop[]asdfghjkl;'zxcvbnm,./qwertyuiop[]asdfghjkl;'zxcvbnm,./qwertyuiop[]asdfghjkl;'zxcvbnm,./qwertyuiop[]asdfghjkl;'zxcvbnm,./qwertyuiop[]asdfghjkl;'zxcvbnm,./qwertyuiop[]asdfghjkl;'zxcvbnm,./",
-			dbSize: 100,
+			ID:       4,
+			Name:     "Too long key",
+			Key:      "qwertyuiop[]asdfghjkl;'zxcvbnm,./qwertyuiop[]asdfghjkl;'zxcvbnm,./qwertyuiop[]asdfghjkl;'zxcvbnm,./qwe",
+			dbSize:   100,
+			Expected: db.ErrKeyTooLong,
 		},
 		TestCase{
-			ID:     5,
-			Name:   "Only numbers",
-			Key:    "124189120",
-			dbSize: 100,
+			ID:       5,
+			Name:     "Only numbers",
+			Key:      "124189120",
+			dbSize:   100,
+			Expected: nil,
 		},
 	}
 	for _, tc := range tcs {
-		a := Hash(tc.Key, tc.dbSize)
+		a, err := Hash(tc.Key, tc.dbSize)
 		b := Hash(tc.Key, tc.dbSize)
+		assert.Equal(t, tc.Expected, err)
 		assert.Equal(t, a, b)
 	}
 }
@@ -73,7 +80,7 @@ func RUnlockTest(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-func Get(t *testing.T) {
+func GetTest(t *testing.T) {
 	type TestCase struct {
 		ID       int
 		Name     string
@@ -121,16 +128,16 @@ func Get(t *testing.T) {
 			Expected: db.ErrInvalidKey,
 		},
 	}
-	for _, j := range tcs {
+	for _, tc := range tcs {
 		db, err := NewDB()
 		assert.NoError(t, err)
 
-		err = db.Put(j.Key, j.Value)
+		err = db.Put(tc.Key, tc.Value)
 		assert.NoError(t, err)
 
-		j.Prepare()
-		value, err := db.Get(j.Key)
-		assert.Equal(t, j.Expected, err)
-		assert.Equal(t, j.Value, value)
+		tc.Prepare()
+		value, err := db.Get(tc.Key)
+		assert.Equal(t, tc.Expected, err)
+		assert.Equal(t, tc.Value, value)
 	}
 }
--- pkg/db/errors.go
+++ pkg/db/errors.go
@@ -4,3 +4,4 @@ import "errors"
 
 var ErrInvalidKey = errors.New("invalid key")
 var ErrInvalidValue = errors.New("invalid Value")
+var ErrKeyTooLong = errors.New("invalid key: the key is too long(length > 100)")
